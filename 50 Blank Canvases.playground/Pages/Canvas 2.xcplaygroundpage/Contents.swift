7de82f8ad7cff3532d11913edf2e245ef4d763fa
Liam Davidson <ldavidson@lcs-mba3335.lcs.on.ca>
2017-10-23 11:27:35 -0400

    It's coming. I have the back circles and now i am fooling around with random values.

--- 50 Blank Canvases.playground/Pages/Canvas 2.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Canvas 2.xcplaygroundpage/Contents.swift
@@ -13,7 +13,7 @@ import Cocoa
 import PlaygroundSupport
 
 // Create canvas
-let canvas = Canvas(width: 400, height: 300)
+let canvas = Canvas(width: 450, height: 300)
 
 /*:
  ## Add your code below
@@ -23,6 +23,79 @@ let canvas = Canvas(width: 400, height: 300)
  Use whitespace and comments as appropriate.
  */
 // Replace this comment with your first comment – what is the goal of the code you're about to write?
+
+
+
+canvas.fillColor = Color.black
+
+canvas.drawShapesWithBorders = false
+
+
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 450, height: 300)
+
+
+canvas.fillColor = Color.init(hue: 30, saturation: 0, brightness: 55, alpha: 100)
+
+
+for x in stride(from: 22, through: 450, by: 45) {
+    
+    for y in stride(from: 0, through: 300, by: 75) {
+    
+    
+    canvas.drawEllipse(centreX: x, centreY: y, width: 45, height: 45)
+    
+    
+    }
+    
+}
+
+// The second offset row of background circles
+
+for a in stride(from: 0, through: 450, by: 45) {
+    
+    for b in stride(from: 38, through: 300, by: 75) {
+        
+        canvas.drawEllipse(centreX: a, centreY: b, width: 45, height: 45)
+        
+        
+        
+    }
+}
+
+
+// inner circles
+
+canvas.fillColor = Color.init(hue: 30, saturation: 0, brightness: 70, alpha: 100)
+
+
+var offset = random(from: 18, toButNotIncluding: 27)
+
+var yoffset = random(from: -4, toButNotIncluding: 5)
+
+
+    
+
+
+canvas.drawEllipse(centreX: offset, centreY: yoffset, width: 35, height: 35)
+
+canvas.translate(byX: 45, byY: 0)
+
+canvas.drawEllipse(centreX: offset, centreY: yoffset, width: 35, height: 35)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
